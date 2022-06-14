36f2eab0795e800a4c28425ec249d201800d5055
Dmytro Zubenko <fokist@outlook.com>
2022-06-15 02:48:13 +0300

    Cups number amount check

--- src/CoffeeMachine/Stage2/ingredientCalculator.js
+++ src/CoffeeMachine/Stage2/ingredientCalculator.js
@@ -1,6 +1,12 @@
 const input = require('prompt-sync')();
 
 let cups = input("Write how many cups of coffee you will need:");
+
+if (isNaN(parseInt(cups))) {
+    console.log("The amount has to be a number");
+    return false;
+}
+
 let water = cups * 200;
 let milk = cups * 50;
 let coffeeBeans = cups * 15;
