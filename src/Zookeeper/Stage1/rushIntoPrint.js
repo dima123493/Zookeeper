a2768687d1d9898f8ae475883d99728d7d38e248
Dmytro Zubenko <fokist@outlook.com>
2022-06-22 19:08:24 +0300

    Delete src/Zookeeper directory

--- src/Zookeeper/Stage1/rushIntoPrint.js
+++
@@ -1,5 +0,0 @@
-console.log("I love animals!");
-console.log("Let's check on the animals...");
-console.log("The deer looks fine.");
-console.log("The bat looks happy.");
-console.log("The lion looks healthy.");
